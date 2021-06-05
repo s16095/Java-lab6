06538d0691052e5029b670c724e6e3b840eac986
s16095 <32592553+s16095@users.noreply.github.com>
2021-06-05 16:30:00 +0200

    Task_1
    Task_1

---
+++ RentalService/src/test/java/pl/pjastk/adrwoj/RentalService/RentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjastk.adrwoj.RentalService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
